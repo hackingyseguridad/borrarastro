a514fc59775ffaeb6e9a14c00eee09de0e68e3e6
hackingyseguridad <antonio_taboada@hotmail.es>
2023-01-20 16:47:59 +0100

    Create borrar.sh

---
+++ borrar.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Borra logs del systema .."
+shred -n 7 -t /var/log/wtmp
+shred -n 7 -t /var/log/lastlog
+shred -n 7 -t /var/log/*.log
+shred -n 7 -t /var/log/*.log.*
+rm /var/log/*.log.*.gz
+rm /var/log/*.gz
+echo "Borra historia de comandos .. "
+echo "" > ~/.bash_history && history -c
+history -cw && exit
