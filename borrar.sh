11453d6e01427d0e10df855e57161fa189933ab1
hackingyseguridad <antonio_taboada@hotmail.es>
2026-02-10 19:36:42 +0100

    Update borrar.sh

--- borrar.sh
+++ borrar.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 echo "Borra logs del systema .."
 rm -rf ~/.cache/*
 sudo rm -rf /tmp/*
@@ -11,9 +12,20 @@ shred -n 7  /var/log/*.log.*
 rm /var/log/*.log.*.gz
 rm /var/log/*.gz
 echo "Borra historia de comandos .. "
-echo "" > ~/.bash_history
 ecjp "" > ~/.zsh_history
+rm -f ~/.zsh_history 2>/dev/null
+rm -f ~/.python_history 2>/dev/null
+rm -rf ~/.cache/* 2>/dev/null
+rm -rf ~/.thumbnails/* 2>/dev/null
+find /var/log -name "*.log" -exec truncate -s 0 {} \; 2>/dev/null
+journalctl --vacuum-time=1s 2>/dev/null
 rm ~/.zsh_history
 rm -f ~/.bash_history
-history -c
-history -cw && exit
+rm -rf /tmp/* 2>/dev/null
+rm -rf /var/tmp/* 2>/dev/null
+echo "" > ~/.bash_history
+echo "" > /var/log/wtmp 2>/dev/null
+echo "" > /var/log/lastlog 2>/dev/null
+echo "" > ~/.bash_history 2>/dev/null
+
+
