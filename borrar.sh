37a0f903789eac31c7d9af18bb5f4aae15215015
hackingyseguridad <antonio_taboada@hotmail.es>
2026-02-10 19:38:48 +0100

    Update borrar.sh

--- borrar.sh
+++ borrar.sh
@@ -21,11 +21,13 @@ find /var/log -name "*.log" -exec truncate -s 0 {} \; 2>/dev/null
 journalctl --vacuum-time=1s 2>/dev/null
 rm ~/.zsh_history
 rm -f ~/.bash_history
-rm -rf /tmp/* 2>/dev/null
-rm -rf /var/tmp/* 2>/dev/null
+rm -rf /tmp/* 
+rm -rf /var/tmp/* 
 echo "" > ~/.bash_history
 echo "" > /var/log/wtmp 2>/dev/null
 echo "" > /var/log/lastlog 2>/dev/null
 echo "" > ~/.bash_history 2>/dev/null
+rm -rf /tmp/* 2>/dev/null
+rm -rf /var/tmp/* 2>/dev/null
 
 
